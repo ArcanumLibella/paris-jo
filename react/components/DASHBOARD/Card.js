6e9778d823ab1de24ddfeb2d48f216dee52d504f
Fiona <fiona@MacBook-Pro-de-Fiona-2.local>
2020-01-10 16:58:30 +0100

    feature: add card content

--- react/components/DASHBOARD/Card.js
+++ react/components/DASHBOARD/Card.js
@@ -3,7 +3,31 @@ import React from 'react'
 const Card = (props) => {
   return(
     <div className={ 'card card--' + props.type }>
-      <div className='card__front'>{'FRONT ' + props.type}</div>
+      {/* CARD-FRONT */}
+      <div className='card__front card-front'>
+        <h3 className='card-front__title card-title'>{props.type}</h3>
+        
+        <div className='card-front__wrapper'>
+          <div className='card-front__count'>
+            <h4 className='card-count'>
+              <span>150</span>
+              {' ' + props.text}
+            </h4>
+          </div>
+
+          <div className='card-front__icon'>
+            <svg className={'icon icon-' + props.type}>
+              <use xlinkHref={'#icon-' + props.type}></use>
+            </svg>
+          </div>
+
+          <div className='card-front__graphic'>
+
+          </div>
+        </div>
+      </div>
+
+      {/* CARD-BACK */}
       <div className='card__back'>{'BACK ' + props.type}</div>
     </div>
   )
--- react/components/DASHBOARD/Dashboard.js
+++ react/components/DASHBOARD/Dashboard.js
@@ -9,11 +9,13 @@ const Dashboard = () => {
       <h2 className='dashboard__title dashboard-title'>Numéro de l'arrondissement</h2>
       
       <div className='dashboard__wrapper cards'>
-        <Card type='hostel'/>
-        <Card type='food'/>
-        <Card type='activities'/>
-        <Card type='nature'/>
+        <Card type='hostel' text='hostel'/>
+        <Card type='food' text='restaurants'/>
+        <Card type='activities' text='point of interest'/>
+        <Card type='nature' text='ha of green space'/>
+
         <Card type='overview'/>
+        
         <Card type='affluence'/>
         <Card type='noise'/>
         <Card type='pollution'/>
