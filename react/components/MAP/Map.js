619892d9b61ed7b77b39573552a9a01e5ebdb1b2
Quentin <quentin.monteferrario@gmail.com>
2020-01-16 14:39:44 +0100

    integration map avec mapBoxe

--- react/components/MAP/Map.js
+++ react/components/MAP/Map.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2FzcDg0NTYiLCJhIjoiY2s1Z29pODllMDliZTNva2V4dWd6dmtxYSJ9.cULX617QPNJrqQjGGU3QzA';
 
 const Map = (props) => {
-    return (
 
-            <svg className={'carte'}>
-                <use xlinkHref={'#carte'}></use>
-            </svg>
+    const [longitude, setLongitude] = useState('5');
+    const [latitude, setLatitude] = useState('34');
+    const [zoom, setZoom] = useState('2');
+    const mapContainer = useRef(null);
 
-    )
-}
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/wasp8456/ck5gonrzl0adu1iqg4mywmqap',
+            center: [longitude, latitude],
+            zoom: zoom
+        });
 
-export default Map
+    });
+
+    return <div ref={el => (mapContainer.current = el)}/>;
+};
+
+export default Map;
