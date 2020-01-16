1589b8928c75b5e95fcdebb2f7c43c54ef63bd21
Quentin <quentin.monteferrario@gmail.com>
2020-01-16 12:07:38 +0100

    ajout map svg

--- react/app.js
+++ react/app.js
@@ -5,12 +5,14 @@ import '../public/assets/css/app.scss'
 // COMPONENTS
 import Menu from './components/MENU/Menu.js'
 import Dashboard from './components/DASHBOARD/Dashboard.js'
+import Map from './components/MAP/Map.js'
 
 function App() {
   return (
     <div className="App">
       <main className='homepage'>
         <Menu />
+        <Map />
         <Dashboard />
       </main>
     </div>
---
+++ react/components/MAP/Map.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Map = (props) => {
+    return (
+
+            <svg className={'carte'}>
+                <use xlinkHref={'#carte'}></use>
+            </svg>
+
+    )
+}
+
+export default Map
