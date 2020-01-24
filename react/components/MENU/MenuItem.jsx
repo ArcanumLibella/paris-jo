9ed10fe145e7f2160582aa0a3e1ef6dba55cbb87
Fiona <fiona@MacBook-Pro-de-Fiona-2.local>
2020-01-24 17:15:38 +0100

    FEATURE: click to close dashboard

--- react/components/MENU/MenuItem.jsx
+++ react/components/MENU/MenuItem.jsx
@@ -8,13 +8,6 @@ import { Home, Sports, Infos } from '../../../public/assets/icons/all-icons'
 const MenuItem = (props) => {
   const { label } = props
 
-  // const [isClicked, setIsClicked] = useState("false")
-
-  // const handleSportDisplay = event => {
-  //   setIsClicked(!isClicked)
-  //   console.log(isClicked);
-  // }
-
   // Function to handle menu icons
   const handleIconsMenu = (label) => {
     if (label === 'home') {
@@ -27,9 +20,6 @@ const MenuItem = (props) => {
   }
 
   return (
-    // <div
-    //   className={isClicked ? "menu__item" : "menu__item is-clicked"}
-    //   onClick={(event) => handleSportDisplay(event)}>
     <div className='menu__item'>
       <div className='menu__icon'>
         {handleIconsMenu(label)}
